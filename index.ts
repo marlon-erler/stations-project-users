2951e0ac180614ba63c83b7629816586eb062e2d
a035 <marlon@erler.cc>
2022-11-21 13:28:44 +0100

    added ls command

--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
 #! /usr/bin/env node
 
-let subcommand = process.argv.splice(0, 1)[0];
+//IMPORTS
+import Fs from "fs/promises";
 
+//MAIN
+let dir = "Users";
+let subcommand = process.argv[2];
+
+//repair
+if (subcommand == "init") {
+	Fs.mkdir(dir);
+	process.exit();
+}
+
+//main
+process.chdir(dir);
 switch (subcommand) {
+	case "ls": {
+		let contents = await Fs.readdir(".");
+		console.log(contents.join("\n"));
+		break;
+	}
 	default: {
-		process.stdout.write("e3");
+		console.log("e3");
 	}
 }
